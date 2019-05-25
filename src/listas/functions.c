20da2709ec0707859355c87be359c04a580f7963
ThiagoFerreiraFarias <thiagoferreirafarias@gmail.com>
2019-05-25 11:43:52 +0100

    #TF# fix bugs e impl busca binária

--- src/listas/functions.c
+++ src/listas/functions.c
@@ -9,41 +9,43 @@ const DataNode dataUtil;
  * @param list
  */
 void Dashboard(List* list){
-    int opc = -1;
-    DataNode data;
-    clearScreen();
-    mostraCabecalho("--- MENU PRINCIPAL ---");
-    mostraOpcao(0,"Sair");
-    mostraOpcao(1,"Inserir Item");
-    mostraOpcao(2,"Remover Item");
-    mostraOpcao(3,"Imprimir Lista");
-    mostraOpcao(4,"Gerar Dados automaticamente");
-    mostraOpcao(5,"Converter Array");
-    mostraRodape("--- MENU PRINCIPAL ---");
-    int opcao = lerInteiro("OPCAO:",0,5);
-    switch(opcao) {
-      case 0 :
-         break;
-      case 1 :
-        pushSingular(list);
+    int opcao = -1;
+    while(opcao != 0){
+        clearScreen();
+        mostraCabecalho("--- MENU PRINCIPAL ---");
+        mostraOpcao(0,"Sair");
+        mostraOpcao(1,"Inserir Item");
+        mostraOpcao(2,"Remover Item");
+        mostraOpcao(3,"Imprimir Lista");
+        mostraOpcao(4,"Gerar Dados automaticamente");
+        mostraOpcao(5,"Converter Array e realizar busca Binária");
+        mostraRodape("--- MENU PRINCIPAL ---");
+        opcao = lerInteiro("OPCAO:",0,5);
+        switch(opcao) {
+          case 0 :
+             break;
+          case 1 :
+            pushSingular(list);
+            getchar();
+            break;
+          case 2 :
+            pop(list);
+            getchar();
+            break;
+          case 3 :
+            printList(list);
+            getchar();
+             break;
+          case 4 :
+            pushAuto(list);
+            getchar();
+            break;
+          case 5 :
+            converteArray(list);
+            getchar();
+            break;
+        }
         getchar();
-        break;
-      case 2 :
-        pop(list);
-        getchar();
-        break;
-      case 3 :
-        printList(list);
-        getchar();
-         break;
-      case 4 :
-        pushAuto(list);
-        getchar();
-        break;
-      case 5 :
-        converteArray(list);
-        getchar();
-        break;
     }
 }
 
@@ -75,8 +77,8 @@ void pushSingular(List* list){
 
     double tamLista = ((sizeof(DataNode)*(list->size))/1073741824);    //equivalente a 1 GB em Bytes
 
-    printf("\nItem adicionado com sucesso\n");
     printf("\nTamanho da lista gerada: %.2f GB\n", tamLista);
+    printf("\nItem adicionado com sucesso!\n");
 
 }
 
@@ -133,6 +135,9 @@ void pop(List* list){
         list->head = pointer->next;
         free(pointer);
         list->size--;
+    }else{
+        printf("\nLista Vazia\n");
+        return;
     }
 }
 
@@ -150,22 +155,21 @@ bool isEmpty(List* list){
  * @param list
  */
 void pushAuto (List* list){
-    long long i;
-    printf("\nInsira a quantidade de registos a serem gerados\n\n");
-    getchar();
-    scanf(" %d", &i);
+	long long i, f;
+	printf("\nInsira a quantidade de registos a serem gerados:\n\n");
+	scanf(" %lld",&i);
 
-    for(int f=0;f<i;f++){
-        DataNode reg;
-        reg.id = f;
-        reg.umidade = genSeqRandom(f*2);
-        reg.temperatura = genSeqRandom(f*3);
-        reg.data = getDataTime();
-        push(list, reg);
-    };
-    double tamLista = ((sizeof(DataNode)*(list->size))/1073741824); //equivalente a 1 GB em Bytes
-    printf("\nTamanho da lista gerada: %.2f GB", tamLista);
-    printf("\nLista gerada com sucesso");
+    DataNode reg;
+	for(f=0;f<i;f++){
+		reg.id = f;
+		reg.umidade = genSeqRandom(f*2);
+		reg.temperatura = genSeqRandom(f*3);
+		reg.data = getDataTime();
+		push(list, reg);
+	}
+	double tamLista = ((sizeof(DataNode)*(list->size))/1073741824); //equivalente a 1 GB em Bytes
+	printf("\nTamanho da lista gerada: %.2f GB", tamLista);
+	printf("\nLista gerada com sucesso\n");
 }
 
 /**
@@ -204,24 +208,60 @@ DReg getDataTime() {
 	return reg;
 }
 
+void buscaBin (DataNode arrayNode[], int arraySize, int buscarId){
+    int idEncontrado = 0;
+	int min = 0;
+	int meio= 0;
+    int max = (arraySize - 1);
+
+    while( (idEncontrado == 0 )  && (max >= min) ){
+        meio = (min + max) / 2;
+        if ( arrayNode[meio].id == buscarId){
+			idEncontrado = 1;
+			printf("\nId encontrado\n");
+			printf("\nPosição no array: %d",meio);
+			printf("\nId: %d", arrayNode[meio].id);
+			printf("\nUmidade: %d", arrayNode[meio].umidade);
+			printf("\nTemperatura: %d", arrayNode[meio].temperatura);
+			printf("\nHora: %d:%d:%d\n", arrayNode[meio].data.hora, arrayNode[meio].data.min, arrayNode[meio].data.seg);
+		} else{
+			if (arrayNode[meio].id > buscarId ){
+				min = meio + 1;
+			} else{
+				max = meio - 1;
+			}
+		}
+    }
+	if (idEncontrado == 0){
+        printf("\nId não encontrado\n");
+	}
+}
+
 /**
- * 
+ *
  * @param list
  */
 void converteArray(List* list) {
-    if(!isEmpty(list)){
-        int arraySize = (int)list->size, i = 0;
-        struct dataNode arrayNode [arraySize];
-        Node* pointer = list->head;
 
-        while(pointer != NULL){			
-                DataNode dataItem = pointer->data;
-                arrayNode[i] = dataItem;
-                pointer = pointer->next;
-                i++;
-        }
-        //TODO: busca binara usando o array, altera o retorno da função
-    } else{
-            printf("\nA lista está vazia.\n");
-    }
+	if(!isEmpty(list)){
+		int arraySize = (int)list->size, i = 0;
+		struct dataNode arrayNode [arraySize];
+		int buscarId = 0;
+		Node* pointer = list->head;
+
+		while(pointer != NULL){
+			DataNode dataItem = pointer->data;
+			arrayNode[i] = dataItem;
+			pointer = pointer->next;
+			//printf("\nId %d",arrayNode[i].id);
+			i++;
+
+		}
+		printf("\nInsira o id do Registo que deseja buscar:\n");
+		scanf("%d",&buscarId);
+		buscaBin(arrayNode, i, buscarId);
+
+	} else{
+		printf("\nA lista está vazia.\n");
+	}
 }
